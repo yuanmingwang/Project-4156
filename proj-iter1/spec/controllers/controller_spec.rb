0284f698d49af6ed4a3109ffcd30691a0f4d4926
Shengkai Li <sl4685@columbia.edu>
2021-03-12 17:00:51 +0000

    spec fail

--- proj-iter1/spec/controllers/controller_spec.rb
+++ proj-iter1/spec/controllers/controller_spec.rb
@@ -4,7 +4,7 @@ describe FluffysController do
   describe "Click on 'Get Rewards to update fluffy_coins'" do
     it 'calls the model method that updates fluffy_coins' do
        expect(Fluffys).to receive(:update_fluffy_coin)
-       get :get_rewards
+       get :get_rewards, {:id => 3}
     end
   end
 end
