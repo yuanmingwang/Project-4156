d8006b049087d749d562c5ecee4b5cae31f877a0
Yuanming Wang <yw3472@columbia.edu>
2021-03-29 19:21:08 +0000

    update database

--- proj-iter1/db/seeds.rb
+++ proj-iter1/db/seeds.rb
@@ -6,16 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-fluffys = [{:name => 'Aladdin', :budget => '1000', :remaining_budget => '800', :fluffy_coin => '0', :password => '0'},
-    	  {:name => 'The Terminator', :budget => '500', :remaining_budget => '400', :fluffy_coin => '0', :password => '0'},
-    	  {:name => 'When Harry Met Sally', :budget => '200', :remaining_budget => '100', :fluffy_coin => '0', :password => '0'},
-      	  {:name => 'The Help', :budget => '3000', :remaining_budget => '1300', :fluffy_coin => '0', :password => '0'},
-      	  {:name => 'Chocolat', :budget => '1200', :remaining_budget => '500', :fluffy_coin => '0', :password => '0'},
-      	  {:name => 'Amelie', :budget => '300', :remaining_budget => '140', :fluffy_coin => '0', :password => '0'},
-      	  {:name => '2001: A Space Odyssey', :budget => '400', :remaining_budget => '250', :fluffy_coin => '0', :password => '0'},
-      	  {:name => 'The Incredibles', :budget => '100', :remaining_budget => '70', :fluffy_coin => '0', :password => '0'},
-      	  {:name => 'Raiders of the Lost Ark', :budget => '50', :remaining_budget => '20', :fluffy_coin => '0', :password => '0'},
-      	  {:name => 'Chicken Run', :budget => '600', :remaining_budget => '550', :fluffy_coin => '0', :password => '0'},
+fluffys = [{:name => 'Allen', :budget => '1000', :remaining_budget => '800', :fluffy_coin => '0', :password => '0'},
+    	  {:name => 'Tom', :budget => '500', :remaining_budget => '400', :fluffy_coin => '0', :password => '0'},
+    	  {:name => 'Winnie', :budget => '200', :remaining_budget => '100', :fluffy_coin => '0', :password => '0'},
+      	  {:name => 'Tod', :budget => '3000', :remaining_budget => '1300', :fluffy_coin => '0', :password => '0'},
+      	  {:name => 'Choco', :budget => '1200', :remaining_budget => '500', :fluffy_coin => '0', :password => '0'},
+      	  {:name => 'Amila', :budget => '300', :remaining_budget => '140', :fluffy_coin => '0', :password => '0'},
+      	  {:name => 'John', :budget => '400', :remaining_budget => '250', :fluffy_coin => '0', :password => '0'},
+      	  {:name => 'Anna', :budget => '100', :remaining_budget => '70', :fluffy_coin => '0', :password => '0'},
+      	  {:name => 'Kevin', :budget => '50', :remaining_budget => '20', :fluffy_coin => '0', :password => '0'},
+      	  {:name => 'Bob', :budget => '600', :remaining_budget => '550', :fluffy_coin => '0', :password => '0'},
   	 ]
 
 events = [{:name => 'Basketball tournament', :location => 'Dodge gym', :interests => 'Sports', :budget => '100', :frequency_of_entertainment => 'twice'},
